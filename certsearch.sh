0f3c59715d6747386525484168fd9636a8ff9910
wifiuk <cn.roberts@me.com>
2017-09-08 15:18:13 +0100

    added cat output to the end of the script

--- certsearch.sh
+++ certsearch.sh
@@ -6,3 +6,6 @@ echo "Parsing input file and running search"
 for DOMAIN in $(cat $1); do
 curl --progress-bar https://crt.sh/?q=%25.$DOMAIN | grep "<TD>" | egrep -v '(<TD><A)' | awk '{ print $1 }' | sed 's/....//' | sed -r 's/.{5}$//' | sort -u >> cert-output.txt
 done
+
+echo "showing results and saving to file"
+cat cert-output.txt
