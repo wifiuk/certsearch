959ac22debf74444d9d0c167b3932f7aff09524b
wifiuk <cn.roberts@me.com>
2017-09-08 13:15:30 +0100

    updated certsearch.ch

--- certsearch.sh
+++ certsearch.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # A quick and dirty script to do a wildcard query on Https://crt.sh to find subdomains using certs for domain
 #
+IFS=$'\n' #make newlines the only separator
 echo "Parsing input file and running search" 
 for DOMAIN in $(cat $1); do
-curl --progress-bar https://crt.sh/?q=%25.$DOMAIN | grep "<TD>" | egrep -v '(<TD><A)' | awk '{ print $1 }' | sed 's/....//' | sed -r 's/.{5}$//' | uniq -u > cert-output.txt
+curl --progress-bar https://crt.sh/?q=%25.$DOMAIN | grep "<TD>" | egrep -v '(<TD><A)' | awk '{ print $1 }' | sed 's/....//' | sed -r 's/.{5}$//' | uniq -u >> cert-output.txt
 done
-echo "Output results to cert-output.txt"
-cat cert-output.txt
